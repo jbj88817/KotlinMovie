5fd9a52faea7a9a5e709bd94ae14956942dc558c
Bojie Jiang <jiangbojie@gmail.com>
2020-05-09 19:58:57 -0700

    Adding network logger

--- app/src/main/java/us/bojie/moviekotlin/data/remote/RetrofitClient.kt
+++ app/src/main/java/us/bojie/moviekotlin/data/remote/RetrofitClient.kt
@@ -1,17 +1,31 @@
 package us.bojie.moviekotlin.data.remote
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    const val BASE_URL = "http://www.omdbapi.com"
+    const val BASE_URL = "https://www.omdbapi.com"
     const val API_KEY = "586573af"
+    private var client: OkHttpClient
+
+    init {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
+
 
     val webservice: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build().create(ApiService::class.java)
     }
+
 }
